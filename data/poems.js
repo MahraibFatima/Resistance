6e1182c150fea561b99ce683291a70741374d5f9
MahraibFatima <mahraibfatima5@gmail.com>
2025-10-05 19:02:29 +0500

    poems added.

---
+++ data/poems.js
@@ -0,0 +1,221 @@
+const poems = [
+    {
+        title: "The War Will End",
+        verses: [
+            "The war will end,",
+            "The leaders will shake hands.",
+            "The old woman will keep waiting for her martyred son.",
+            "That girl will wait for her beloved husband,",
+            "And those children will wait for their heroic father.",
+            "I don't know who sold our homeland,",
+            "But I saw who paid the price."
+        ],
+        author: "Mahmoud Darwish"
+    },
+    {
+        title: "What Would You Do?",
+        verses: [
+            "What would you do,",
+            "if they kicked you out of your house,",
+            "took away your land,",
+            "closed your businesses,",
+            "bombed and killed your family members,",
+            "your spouses and children,",
+            "threw your family and friends in jail?",
+            "What if they occupied your country,",
+            "took away everything you ever knew,",
+            "and erased your identity in front of your eyes?",
+            "What would you do?",
+            "if you saw all these injustices,",
+            "only to realize that",
+            "the whole world is not on your side?",
+            "What would you do?",
+            "if they did all that, and even more,",
+            "and not for one week, but for over 70 years?",
+            "Ask yourself this:",
+            "What would you do?"
+        ],
+        author: "Anonymous"
+    },
+    {
+        title: "The Unbroken Chain",
+        verses: [
+            "How can I sleep.",
+            "when my brothers and sisters are",
+            "suffering, tortured,",
+            "murdered, annihilated..",
+            "This is a genocide in the most evil form,",
+            "and the world is just watching.",
+            "Allah knows and sees,",
+            "and He delays them until a day known only to Him."
+        ],
+
+        author: "Anonymous"
+    },
+    {
+        title: "I am a child of Palestine.",
+        verses: [
+            "I am silenced and beaten.",
+            "I am a child of Palestine.",
+            "I am ignored and forgotten.",
+            "I am unimportant.",
+            "I am irrelevant.",
+            "I am a child of Palestine.",
+            "I am tortured and imprisoned.",
+            "I am battered and frightened.",
+            "I have no bombs,",
+            "No weapons.",
+            "Yet, you call it a war.",
+            "I am a child of Palestine.",
+            "Turn back, look away,",
+            "As the bodies fall.",
+            "Watch me suffer.",
+            "Hear me cry.",
+            "Only because I am a child of Palestine.",
+
+        ],
+        author: "Gulrez Shagufa"
+    },
+    {
+        title: "We will rise again and again.",
+        verses: [
+            "They say the war will end one day.",
+            "But sorrow never fades away.",
+            "Leaders smile and shake hands,",
+            "while our pain forever stands",
+
+            "A mother waits for her child,",
+            "in a world gone so wild.",
+            "An old man yearns for peace,",
+            "but finds only war's cruel lease.",
+
+            "They talk of freedom and rights,",
+            "while we endure endless nights.",
+            "Yet, through our tears and pain.",
+            "We will rise again and again.",
+
+        ],
+        author: "Sidraacore"
+    },
+    {
+        title: "He will never leave you.",
+        verses: [
+            "Tears shed.",
+            "People are martyred.",
+            "Many words remains unsaid.",
+            "Those white walls are suddenly red",
+            "And everywhere is blood,",
+            "everywhere is blood.",
+            "Yet, with hope we go ahead",
+            "Do not think your voice is not heard",
+            "Surely, your lord will never leave you unhelped,",
+            "He will never leave you unhelped.",
+
+        ],
+        author: "Sumaiya.A"
+    },
+    {
+        title: "To Our Land",
+        verses: [
+            "To our land,",
+            "and it is the one near the word of God,",
+            "a ceiling of clouds...",
+            "To our land,",
+            "and it is the one far from the adjectives of nouns,",
+            "the map of absence...",
+            "To our land,",
+            "and it is the one tiny as a sesame seed,",
+            "a heavenly horizon... and a hidden chasm...",
+            "To our land,",
+            "and it is the one poor as a grouse's wings,",
+            "holy books... and an identity in a wound...",
+            "To our land,",
+            "and it is the one surrounded with torn hills,",
+            "the ambush of a new past...",
+            "To our land, and it is the prize of war,",
+            "the freedom to die from longing and burning...",
+            "And our land, in its bloodied night,",
+            "is a jewel that glimmers for the far upon the far,",
+            "and illuminates what's outside it...",
+            "As for us, inside,",
+            "we suffocate more!"
+        ],
+        author: "Mahmoud Darwish"
+    },
+    {
+        title: "If I Must Die",
+        verses: [
+            "If I must die,",
+            "you must live.",
+            "to tell my story,",
+            "to sell my things,",
+            "to buy a piece of cloth,",
+            "and some strings,",
+            "(make it white with a long tail).",
+            "So that a child, somewhere in Gaza,",
+            "while looking heaven in the eye,",
+            "awaiting his dad who left in a blaze.",
+            "And bid no one farewell,",
+            "not even to his flesh,",
+            "not even to himself.",
+            "Sees the kite, my kite you made,",
+            "flying up above,",
+            "and thinks for a moment.",
+            "An angel is there,",
+            "bringing back love.",
+            "If I must die,",
+            "let it bring hope.",
+            "let it be a tale."
+        ],
+        author: "Refaat Alareer"
+    },
+
+    {
+        title: "Our Loneliness",
+        verses: [
+            "In gardens where our dreams take root,",
+            "We plant the seeds of hope and truth.",
+            "With tender care and gentle hands,",
+            "We nurture life across the lands.",
+            "",
+            "Each flower blooms with colors bright,",
+            "Reflecting love and inner light.",
+            "In this garden, all are free,",
+            "To be the person they should be."
+        ],
+        author: "",
+        author: "Hiba.A.N Translated by Salma.H"
+    },
+    {
+        title: "This Bread Was Born, This Bread Was Killed",
+        verses: [
+            "With clean hands,",
+            "he gently sifts the flour,",
+            "and adds a handful of yeast.",
+            "He pours the warm water,",
+            "for the yeast particles to live.",
+            "Then, rolls and kneads and rolls",
+            "and kneads the dough.",
+            "He lets the soft mass rest.",
+            "With firm but gentle hands,",
+            "he rounds it into balls,",
+            "flattens them into shape,",
+            "and handles each one",
+            "delicately into the oven.",
+            "Soon, perhaps in half an hour,",
+            "the bread rolls are born:",
+            "fresh, healthy and browned.",
+            "The newborn breads breathe,",
+            "yet dust chokes the air,",
+            "searing gases penetrate",
+            "their thin, fragile crusts.",
+            "On the day of their birth, a missile—",
+            "a bakery, a scattering",
+            "of zaatar, flesh, and blood."
+        ],
+        author: "Basman Aldirawi"
+    }
+];
+
+// Expose to global scope
+window.poems = poems;
